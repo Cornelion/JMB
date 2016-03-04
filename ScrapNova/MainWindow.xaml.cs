14a5ea450a4eb350244ed06f4384d9ee6b55e85c
Cornelion <eric_philippe_@hotmail.com>
2016-03-04 16:46:18 +0100

    Vendredi soir

--- ScrapNova/MainWindow.xaml.cs
+++ ScrapNova/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,12 +27,30 @@ namespace ScrapNova
         {
             InitializeComponent();
             List<Evenement> evenements = new List<Evenement>();
+         
+           // XElement x = XElement.Load("http://www.nova-cinema.org/seances");
 
-            //evenements.Add(new Evenement() { Date = "25-01", Heure = "20h", Titre = "super-film", Description = "un très super film" });
-            //evenements.Add(new Evenement() { Date = "26-01", Heure = "20h", Titre = "film naze", Description = "ne venez pas" });
+            WebRequest request = WebRequest.Create("http://www.nova-cinema.org/seances");
+            WebResponse response = request.GetResponse();
+            Stream data = response.GetResponseStream();
+            string html = String.Empty;
+            using (StreamReader sr = new StreamReader(data))
+            {
+                html = sr.ReadToEnd();
+            }
+            string first = @"class=""menu-entree item"">";
+            string second = "Ouloulou";
+            html = html.Replace(first, second);
+                                
+            //XElement x = XElement.Parse(html);
+            //var container = from elem in x.Elements()
+            //                let attr = elem.Attribute("class")
+            //                where attr.Equals("rub2157")
+            //                select (string)elem;
+            //string text="";
+            //foreach(string s in container) { text += s; }
 
-            XElement x = XElement.Load("http://www.nova-cinema.org/seances");
-            
+            texttest.Text = html;
             thegrid.ItemsSource = evenements;
         }
     }
