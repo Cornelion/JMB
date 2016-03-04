e7772147dde2da7c16d08f671536214f0ae0565f
Cornelion <eric_philippe_@hotmail.com>
2016-03-04 14:32:22 +0100

    First!

---
+++ ScrapNova/Evenement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapNova
+{
+    public class Evenement
+    {
+        public string Lieu { get; set; }
+        public string Date { get; set; }
+        public string Heure { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        //public Evenement( string date,string heure, string titre,string description)
+        //{
+
+        //    Date = date;
+        //    Heure = heure;
+        //    Titre = titre;
+        //    Description = description;
+        //}
+    }
+}
---
+++ ScrapNova/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ScrapNova
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Evenement> evenements = new List<Evenement>();
+
+            //evenements.Add(new Evenement() { Date = "25-01", Heure = "20h", Titre = "super-film", Description = "un très super film" });
+            //evenements.Add(new Evenement() { Date = "26-01", Heure = "20h", Titre = "film naze", Description = "ne venez pas" });
+            XElement x = XElement.Load("http://www.nova-cinema.org/seances");
+            
+            thegrid.ItemsSource = evenements;
+        }
+    }
+}
