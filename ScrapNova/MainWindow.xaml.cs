6a5bfee860b2c759038051ee6cb07dfd2235b337
Cornelion <eric_philippe_@hotmail.com>
2016-03-08 14:03:03 +0100

    WorkingNova

--- ScrapNova/MainWindow.xaml.cs
+++ ScrapNova/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,6 +32,7 @@ namespace ScrapNova
          
            // XElement x = XElement.Load("http://www.nova-cinema.org/seances");
 
+
             WebRequest request = WebRequest.Create("http://www.nova-cinema.org/seances");
             WebResponse response = request.GetResponse();
             Stream data = response.GetResponseStream();
@@ -38,11 +41,20 @@ namespace ScrapNova
             {
                 html = sr.ReadToEnd();
             }
-            string first = @"class=""menu-entree item"">";
-            string second = "Ouloulou";
-            html = html.Replace(first, second);
-                                
-            //XElement x = XElement.Parse(html);
+
+            //Match truc = Regex.Match(html, @"<li[^A-Za-z0-9_]?class=""menu-entree item"">[^A-Za-z0-9_]*<li[^A-Za-z0-9_]?class=""menu-entree item"">"); 
+
+
+            //CORRIGER LA PREMIERE ERREURXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            //string  htmlcut= Regex.Replace(html, @"<li[^A-Za-z0-9_]?class=""menu-entree item"">[^A-Za-z0-9_]*<li[^A-Za-z0-9_]?class=""menu-entree item"">", @"<li class=""menu-entree item"">");
+
+            //167---3279
+            // Match truc = Regex.Match(html, @"<div class=""hentry"" id=""contenu"">.*</div><div style=""clear:both""></div>[^A-Za-z0-9_]*</li>[^A-Za-z0-9_]*</ul>[^A-Za-z0-9_]*</div>[^A-Za-z0-9_]*</div>",RegexOptions.Singleline);
+            //TRIMERXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            //Match truc = Regex.Match(html, @"<div[^A-Za-z0-9_]?class=""hentry""[^A-Za-z0-9_]?id=""contenu"">.*</div><div[^A-Za-z0-9_]?style=""clear:both""></div>[^A-Za-z0-9_]*</li>[^A-Za-z0-9_]*</ul>[^A-Za-z0-9_]*</div>[^A-Za-z0-9_]*</div>", RegexOptions.Singleline);
+            //string htmlcut = truc.Value;
+            //texttest.Text = htmlcut;
+            // XElement x = XElement.Parse(@"<!DOCTYPE html PUBLIC  + ""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"">"+truc.Value);
             //var container = from elem in x.Elements()
             //                let attr = elem.Attribute("class")
             //                where attr.Equals("rub2157")
@@ -50,7 +62,34 @@ namespace ScrapNova
             //string text="";
             //foreach(string s in container) { text += s; }
 
-            texttest.Text = html;
+            var htmldoc = new HtmlDocument();
+            //htmldoc.LoadHtml(new WebClient().DownloadString("http://www.nova-cinema.org/seances"));
+            WebClient wb = new WebClient();
+            htmldoc.Load(wb.OpenRead("http://www.nova-cinema.org/seances"), Encoding.UTF8);
+            
+            var root = htmldoc.DocumentNode;
+            var mars = root.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("liste agenda rub2157")).Single();
+            foreach(HtmlNode x in mars.Descendants().Where(n=>n.GetAttributeValue("class","").Equals("item") ) )
+            {
+                string temp = String.Empty;
+                Evenement Ev = new Evenement() { Lieu = "Nova" };
+
+                HtmlNode y = x.Descendants("span").Where(n => n.GetAttributeValue("class", "").Equals("date")).Single();
+                string[] st = y.InnerText.Split(' ');
+                
+                for(int i = 0; i < st.Length-1; i++) { temp += (st[i]); };
+                Ev.Date = temp.Trim().clean();
+                Ev.Heure = st[st.Length - 1].Trim().clean();
+               
+
+                y = x.Descendants("span").Where(n => n.GetAttributeValue("class", "").Equals("titre")).Single();
+                temp = (y.InnerText).Substring(1);
+                Ev.Titre =temp.clean();
+                y = x.Descendants("div").Where(n => n.GetAttributeValue("class", "").Equals("descriptif court")).Single();
+                Ev.Description = y.InnerText.clean();
+                evenements.Add(Ev);
+            }
+           // texttest.Text = mars.InnerHtml;
             thegrid.ItemsSource = evenements;
         }
     }
---
+++ ScrapNova/StringHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace ScrapNova
+{
+    public static class StringHelpers
+    {   
+        public static string clean(this String s)
+        {
+            s = Regex.Replace(s, "&#8217;", "'");
+            return s = Regex.Replace(s, "&nbsp;", "");
+        }
+    }
+}
